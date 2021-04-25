181e2bc2d11313c016ce9e99916050f6faba048f
Akash Kuratkar <akashkuratkar@Akashs-New-MacBook-Pro.local>
2021-04-25 09:28:16 -0700

    Redux bug fix

--- splitwise-client/src/redux/reducers/Userreducer.js
+++ splitwise-client/src/redux/reducers/Userreducer.js
@@ -1,26 +1,28 @@
 /* eslint-disable no-param-reassign */
 /* eslint-disable no-return-assign */
 
-export default function userLogin(state = {}, action) {
+export default function userLogin(state = [], action) {
   switch (action.type) {
     case 'LOGIN_USER':
       return Object.assign(...state, action.payload);
     case 'LOGIN_REQUEST':
       return Object.assign(...state, action.payload);
     case 'LOGIN_USER_SUCCESS':
-      return action.payload;
+      return Object.assign((state, action.payload));
     case 'LOGIN_USER_FAILURE':
       return Object.assign(state, { ...action.payload });
     case 'SIGN_UP_SUCCESS':
-      return Object.assign(state, { ...action.payload });
+      return Object.assign((state, action.payload));
     case 'SIGN_UP_FAILURE':
-      return Object.assign(state, action.payload);
+      return Object.assign(...state, ...action.payload);
     case 'USER_PROFILE_SUCCESS':
       return Object.assign(state, { ...action.payload });
     case 'USER_SUCCESS':
       return Object.assign(state, action.payload);
     case 'USER_PROFILE_FAILURE':
       return Object.assign(...state, action.payload);
+    case 'LOGOUT_USER':
+      return (state = []);
     default:
       return state;
   }
